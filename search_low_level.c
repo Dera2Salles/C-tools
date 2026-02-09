ccf5252d9c168b4ef2398df2c033fcd18b674370
Dera2S <dera.ah.14@gmail.com>
2026-02-09 16:48:53 +0300

    Update search_low_level

--- search_low_level.c
+++ search_low_level.c
@@ -1,5 +1,6 @@
 #include <fcntl.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 #define BUF_SIZE 20
@@ -7,7 +8,7 @@
 
 int main(int argc, char *argv[]) {
 
-  int fd = open("out/toto.txt", O_RDONLY);
+  int fd = open("out/toto.txt", O_RDWR);
 
   if (fd == -1) {
     perror("open");
@@ -16,17 +17,30 @@ int main(int argc, char *argv[]) {
 
   char buffer[BUF_SIZE];
   char word[WORD_SIZE];
+  char search[] = "toto";
   int wpos = 0;
   ssize_t byte_read;
+  int stop = 0;
 
   while ((byte_read = read(fd, buffer, BUF_SIZE)) > 0) {
+    if (stop) {
+      break;
+    }
     for (int i = 0; i < byte_read; i++) {
       char caracter = buffer[i];
       if (caracter == ' ' || caracter == '\n' || caracter == '\t' ||
           caracter == '.' || caracter == ',') {
         if (wpos > 0) {
           word[wpos] = '\0';
-          printf("Mot trouvé avec espace: %s\n", word);
+          if (strcmp(search, word) == 0) {
+            printf("--%s--", buffer);
+
+            printf("index %d", i);
+            lseek(fd, -(byte_read - i + wpos), SEEK_CUR);
+            write(fd, "niova", strlen("niova"));
+            stop = 1;
+            break;
+          }
           wpos = 0;
         }
       } else {
