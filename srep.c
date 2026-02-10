e7366973409e630477fc4f73f79d39b795c0176f
Dera2S <dera.ah.14@gmail.com>
2026-02-10 15:41:36 +0300

    Update for more flexibility

--- srep.c
+++ srep.c
@@ -1,63 +1,79 @@
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-#define BUF_SIZE 20
-#define WORD_SIZE 64
+#define BUF_SIZE 32
 
-int isEndOfSentence(char c) {
+int isSeparator(char c) {
   return (c == ' ' || c == '\n' || c == '\t' || c == '.' || c == ',');
 }
 
 int main(int argc, char *argv[]) {
-
   if (argc < 4) {
-    fprintf(
-        stderr,
-        "Usage: %s <fichier> <mot_a_chercher> <mot_de_remplacement>\n"
-        "Example:\n"
-        "  %s out/toto.txt toto niov\n\n"
-        "  Attention: remplacement par écrasement (même taille recommandée)\n",
-        argv[0], argv[0]);
+    fprintf(stderr,
+            "Usage: %s <fichier> <mot_a_chercher> <mot_de_remplacement>\n"
+            "Example:\n  %s out/toto.txt toto niov\n",
+            argv[0], argv[0]);
     return 1;
   }
 
-  int fd = open(argv[1], O_RDWR);
-  if (fd == -1) {
+  char *filename = argv[1];
+  char *search = argv[2];
+  char *replace = argv[3];
+
+  int fd = open(filename, O_RDONLY);
+  int tmp = open("temp.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+  if (fd == -1 || tmp == -1) {
     perror("open");
     return 1;
   }
 
   char buffer[BUF_SIZE];
-  char word[WORD_SIZE];
+  char *word = malloc(1);
   int wpos = 0;
+  int word_size = 1;
   ssize_t byte_read;
 
   while ((byte_read = read(fd, buffer, BUF_SIZE)) > 0) {
     for (int i = 0; i < byte_read; i++) {
-      char caracter = buffer[i];
+      char c = buffer[i];
 
-      if (isEndOfSentence(caracter)) {
+      if (isSeparator(c)) {
         if (wpos > 0) {
           word[wpos] = '\0';
-
-          if (strcmp(argv[2], word) == 0) {
-            lseek(fd, -(byte_read - i + wpos), SEEK_CUR);
-
-            write(fd, argv[3], strlen(argv[3]));
+          if (strcmp(word, search) == 0) {
+            write(tmp, replace, strlen(replace));
+          } else {
+            write(tmp, word, strlen(word));
           }
-
           wpos = 0;
         }
+        write(tmp, &c, 1);
       } else {
-        if (wpos < WORD_SIZE - 1) {
-          word[wpos++] = caracter;
+        if (wpos + 1 >= word_size) {
+          word_size *= 2;
+          word = realloc(word, word_size);
         }
+        word[wpos++] = c;
       }
     }
   }
 
+  if (wpos > 0) {
+    word[wpos] = '\0';
+    if (strcmp(word, search) == 0)
+      write(tmp, replace, strlen(replace));
+    else
+      write(tmp, word, strlen(word));
+  }
+
+  free(word);
   close(fd);
+  close(tmp);
+
+  unlink(filename);
+  rename("temp.txt", filename);
   return 0;
 }
